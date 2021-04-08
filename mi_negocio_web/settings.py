893bd60e4227f441eba4c209c39c7921eba10766
Oscar Yañez <o.yg@hotmail.com>
2021-04-07 20:31:31 -0500

    aplicacion de carrito lista

--- mi_negocio_web/settings.py
+++ mi_negocio_web/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'nucleo',
     'tienda',
+    'carrito',
     
 ]
 
@@ -66,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carrito.procesador_contexto.carrito',
+
             ],
         },
     },
